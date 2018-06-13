822fd74d5bee4c4b0e042be0ee72f04b7387f943
Bour <18503818578@163.com>
2018-06-13 16:49:49 +0800

    nothing

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,16 @@ use std::cell::RefCell;
 use std::vec::Vec;
 use std::rc::Weak;
 
-use sdl_test::sprite::{ Sprite , Drawable,EventHandle,BV,DH};
+use sdl_test::sprite::{Sprite, Drawable, EventHandle, BV, DH};
 
-const START_W:u32 = 180;
-const START_H:u32 = 80;
+const START_W: u32 = 180;
+const START_H: u32 = 80;
 
-const W:u32 = 800;
-const H:u32 = 600;
+const W: u32 = 800;
+const H: u32 = 600;
 
 pub fn run(png: &Path) {
-    let sprites : Rc<RefCell<Vec<RefCell<Box<DH <Target=WindowCanvas>>>>>> = Rc::new(RefCell::new(Vec::new()));
+    let sprites: Rc<RefCell<Vec<RefCell<Box<DH<Target=WindowCanvas>>>>>> = Rc::new(RefCell::new(Vec::new()));
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -49,20 +49,20 @@ pub fn run(png: &Path) {
     };
 
 
-    let new_surface = match surface.zoom(0.3, 0.3, false) {
+    let new_surface = match surface.zoom(0.4, 0.4, false) {
         Ok(su) => {
             su
-        },
+        }
         Err(e) => {
             panic!("failed to zoom surface: {}", e)
         }
     };
 
-    let start_surface =  match Surface::from_file("resource/start.png") {
+    let start_surface = match Surface::from_file("resource/start.png") {
         Ok(surface) => surface,
         Err(err) => panic!("failed to load cursor image: {}", err)
     };
-    let start_texture = match  texture_creator.create_texture_from_surface(start_surface) {
+    let start_texture = match texture_creator.create_texture_from_surface(start_surface) {
         Ok(texture) => texture,
         Err(e) => panic!("failed to create start texture : {}", e)
     };
@@ -73,7 +73,7 @@ pub fn run(png: &Path) {
     };
     cursor.set();
 
-    let start_sprite = create_start(start_texture,Rc::downgrade(&sprites));
+    let start_sprite = create_start(start_texture, Rc::downgrade(&sprites));
     (*sprites).borrow_mut().push(RefCell::new(Box::new(start_sprite)));
 
 
@@ -83,17 +83,17 @@ pub fn run(png: &Path) {
     let mut events = sdl_context.event_pump().unwrap();
 
     'mainloop: loop {
-        let mouse_pos = (events.mouse_state().x(),events.mouse_state().y());
+        let mouse_pos = (events.mouse_state().x(), events.mouse_state().y());
         let temp = (*sprites).borrow();
         for event in events.poll_iter() {
-            for i in 0..(*sprites).borrow().len(){
-                if temp[i].borrow().is_visible() && temp[i].borrow().in_bound(mouse_pos){
+            for i in 0..(*sprites).borrow().len() {
+                if temp[i].borrow().is_visible() && temp[i].borrow().in_bound(mouse_pos) {
                     temp[i].borrow().on_handle_event(&event);
                 }
             }
             match event {
-                Event::Quit{..} |
-                Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Option::Some(Keycode::Escape), .. } =>
                     break 'mainloop,
                 _ => {}
             }
@@ -101,46 +101,45 @@ pub fn run(png: &Path) {
 
         canvas.clear();
 
-        for i in 0..(*sprites).borrow().len(){
-            if temp[i].borrow_mut().is_visible(){
+        for i in 0..(*sprites).borrow().len() {
+            if temp[i].borrow_mut().is_visible() {
                 temp[i].borrow_mut().draw(&mut canvas);
             }
         }
         canvas.present();
-
     }
-    println!("end  {}",Rc::strong_count(&sprites));
+    println!("end  {}", Rc::strong_count(&sprites));
 }
 
-fn create_start(te : Texture,sps : Weak<RefCell<Vec<RefCell<Box<DH <Target=WindowCanvas>>>>>>) -> Sprite
+fn create_start(te: Texture, sps: Weak<RefCell<Vec<RefCell<Box<DH<Target=WindowCanvas>>>>>>) -> Sprite
 {
-    let dst = Rect::new(((W - START_W) / 2) as i32,((H - START_H) / 2) as i32,START_W as u32,START_H as u32);
-    let mut start = Sprite::new(None,Some(dst),te,"start");
+    let dst = Rect::new(((W - START_W) / 2) as i32, ((H - START_H) / 2) as i32, START_W as u32, START_H as u32);
+    let mut start = Sprite::new(None, Some(dst), te, "start");
 
-    start.setEventFunc(Box::new(move |e:&Event,s:&Sprite|{
-        if let Some(sp_up) = sps.upgrade(){
+    start.setEventFunc(Box::new(move |e: &Event, s: &Sprite| {
+        if let Some(sp_up) = sps.upgrade() {
             let temp = (*sp_up).borrow();
-            println!("{}",temp[0].borrow().is_visible());
+            println!("{}", temp[0].borrow().is_visible());
         }
         match *e {
-            Event::MouseButtonDown {mouse_btn,..} => {
+            Event::MouseButtonDown { mouse_btn, .. } => {
                 match mouse_btn {
                     MouseButton::Left => {
                         let nw = START_W - 10;
                         let nh = START_H - 4;
-                        let n_dst = Rect::new(((W - nw ) / 2) as i32,((H - nh) / 2) as i32,nw as u32,nh as u32);
-                        unsafe {(*s.getRefMut()).dst = Some(n_dst);}
-                    },
+                        let n_dst = Rect::new(((W - nw) / 2) as i32, ((H - nh) / 2) as i32, nw as u32, nh as u32);
+                        unsafe { (*s.getRefMut()).dst = Some(n_dst); }
+                    }
                     _ => {}
                 }
-            },
-            Event::MouseButtonUp {mouse_btn,..} => {
+            }
+            Event::MouseButtonUp { mouse_btn, .. } => {
                 match mouse_btn {
                     MouseButton::Left => {
-                        let dst = Rect::new(((W - START_W) / 2) as i32,((H - START_H) / 2) as i32,START_W as u32,START_H as u32);
-                        unsafe {(*s.getRefMut()).dst = Some(dst);}
-                    },
-                    _ =>{}
+                        let dst = Rect::new(((W - START_W) / 2) as i32, ((H - START_H) / 2) as i32, START_W as u32, START_H as u32);
+                        unsafe { (*s.getRefMut()).dst = Some(dst); }
+                    }
+                    _ => {}
                 }
             }
             _ => {}
